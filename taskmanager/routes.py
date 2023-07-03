9e06f53b08030ffc29217201a2f660bd3adc9e9f
Adriele Lima <adriele.p.t@hotmail.com>
2023-07-03 21:05:16 +0000

    Adding read function for Tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,11 +5,14 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
 @app.route("/categories")
 def categories():
+
     categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("categories.html", categories=categories)
 
@@ -28,11 +31,14 @@ def add_category():
 
 @app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
+
     category = Category.query.get_or_404(category_id)
+
     if request.method == "POST":
         category.category_name = request.form.get("category_name")
         db.session.commit()
         return redirect(url_for("categories"))
+
     return render_template("edit_category.html", category=category)
 
 
@@ -42,3 +48,23 @@ def delete_category(category_id):
     db.session.delete(category)
     db.session.commit()
     return redirect(url_for("categories"))
+
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    
+    categories = list(Category.query.order_by(Category.category_name).all())
+
+    if request.method == "POST":
+        task = Task(
+            task_name = request.form.get("task_name"),
+            task_description = request.form.get("task_description"),
+            is_urgent = bool(True if request.form.get("is_urgent") else False),
+            due_date = request.form.get("due_date"),
+            category_id = request.form.get("category_id")
+        )
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("add_task.html", categories=categories)
